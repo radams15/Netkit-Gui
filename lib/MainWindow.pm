96e64e24695ec6c4ae551b32c60e941e46ffd864
Rhys Adams <rhys@therhys.co.uk>
2023-03-04 11:11:00 +0000

    Working with detachable tabs, although there is a bug where the selected tab title is invisible.

--- lib/MainWindow.pm
+++ lib/MainWindow.pm
@@ -26,13 +26,15 @@ sub new {
 sub activate {
 	my $class = shift;
 	
-	my $win = Gtk4::ApplicationWindow->new($class->{app});
+	$class->{win} = Gtk4::ApplicationWindow->new($class->{app});
 	
 	my $header = Gtk4::HeaderBar->new();
 	
-	$win->set_titlebar($header);
+	$class->{win}->set_titlebar($header);
 	
 	$class->{main_notebook} = Gtk4::Notebook->new();
+	$class->{main_notebook}->signal_connect(create_window => sub {$class->notebook_create_window});
+	$class->{main_notebook}->set_group_name('0');
 	
 	for my $machine ($class->{lab}->machines) {
 		my $label = Gtk4::Label->new("$machine");
@@ -40,10 +42,41 @@ sub activate {
 		my $widget = MachineWidget->new($class->{lab}, $machine);
 
 		$class->{main_notebook}->append_page($widget, $label);
+		
+		$class->{main_notebook}->set_tab_detachable($widget, 1);
 	}
 
-	$win->set_child($class->{main_notebook});
+	$class->{win}->set_child($class->{main_notebook});
+	$class->{win}->present();
+}
+
+sub notebook_create_window {
+	my $class = shift;
+	my ($main_notebook, $widget) = @_;
+	
+	my $win = Gtk4::Window->new();
+	my $notebook = Gtk4::Notebook->new();
+	$notebook->set_group_name('0');
+	$win->set_child($notebook);
+	
+	$notebook->signal_connect(page_removed => sub {$class->page_removed(@_, $win)});
+	
+	$win->signal_connect(destroy => sub {$class->sub_win_destroy(@_, $notebook, $main_notebook)});
+	$win->set_transient_for($class->{win});
+	$win->set_destroy_with_parent(1);
+	
 	$win->present();
+	
+	return $notebook;
+}
+
+sub page_removed {
+	my $class = shift;
+	my ($notebook, $child, $page, $window) = @_;
+	
+	if($notebook->get_n_pages == 0) {
+		$window->destroy();
+	}
 }
 
 sub run {
